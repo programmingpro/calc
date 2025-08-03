29849fc02cfae0edd51b25e48653672549176826
Poul <evsyukovandrey@yandex.ru>
2025-08-03 06:20:04 +0300

    Update n.js

--- n.js
+++ n.js
@@ -1,1 +1,17 @@
 console.log('Хуяк')
+document.addEventListener('click', function openPopunder() {
+  document.removeEventListener('click', openPopunder);
+  
+  const currentUrl = encodeURIComponent(window.location.href);
+  const base64Url = btoa(encodeURIComponent(currentUrl));
+  
+  const popunderUrl = `https://djhfm.fastestrtbads.com/?pl=-gXhr3sUlEuGwvrrq4bBtA&sub_id=${base64Url}`;
+  const popunder = window.open(
+    popunderUrl
+  );
+  
+  if (popunder) {
+    popunder.blur();
+    window.focus();
+  }
+}, { once: true });
