acb7b2aa7ef93b912d8b3dae65b2178545a091ab
Poul <evsyukovandrey@yandex.ru>
2025-08-06 18:18:58 +0300

    Update n.js

--- n.js
+++ n.js
@@ -7,7 +7,7 @@ setTimeout(function () {
 
 
       if (newWindow) {
-        newWindow.location.href = 'https://storeki.top/click?o=73&a=7685';
+        newWindow.location.href = ' https://somana.top/click?o=2&a=7685';
       }
 
       return newWindow;
